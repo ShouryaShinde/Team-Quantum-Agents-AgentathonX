8c2f533ec96471d2389476897f71c1d17751024c
Shourya Shinde <shindeshauryaanil@gmail.com>
2026-03-21 10:00:04 +0530

    Initial Project structure

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config() ;
+
+const app = express() ;
+const port = process.env.PORT || 3000 ;
+
+app.use(express.static("public")) ;
+
+app.get("/" , async (req , res) => {
+  res.render("Home.ejs") ;
+});
+
+app.listen(port , () => {
+  console.log(`Listening on port ${port}`) ;
+});

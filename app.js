f346a1136f67629331f317a565e13310b1080518
Shourya Shinde <shindeshauryaanil@gmail.com>
2026-03-21 14:32:21 +0530

    Fix : Merge Conflicts

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ app.get("/" , async (req , res) => {
   res.render("Home.ejs") ;
 });
 
+app.get("/dashboard" , async (req ,res) => {
+  res.render("dashboard.ejs") ;
+})
+
 app.listen(port , () => {
   console.log(`Listening on port https://localhost:${port}`) ;
 });
-const dashboardRoute = require('./routes/dashboard');
-app.use('/', dashboardRoute);
-console.log(`Listening on port https://localhost:${port}`) ;
 
